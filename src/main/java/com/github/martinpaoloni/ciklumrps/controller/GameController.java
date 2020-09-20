d3e952dab7d522b453fc8ccd0e2e1235fa8203a4
Martin Paoloni <martinpaoloni@gmail.com>
2020-09-20 18:12:31 -0300

    Add @RequestMapping to GameController

--- src/main/java/com/github/martinpaoloni/ciklumrps/controller/GameController.java
+++ src/main/java/com/github/martinpaoloni/ciklumrps/controller/GameController.java
@@ -13,42 +13,43 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@RequestMapping("/api/game")
 public class GameController {
 
     private Map<Integer, Game> games = new ConcurrentHashMap<>();
     private AtomicInteger lastIdUsed = new AtomicInteger();
     private Stats globalStats = new Stats();
 
-    @GetMapping(value = "/api/game", produces = "application/json")
+    @GetMapping(produces = "application/json")
     public Collection<Game> listGames() {
         return games.values();
     }
 
-    @GetMapping(value = "/api/game/stats", produces = APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/stats", produces = APPLICATION_JSON_VALUE)
     public Stats stats() {
         return globalStats;
     }
 
-    @GetMapping(value = "/api/game/{id}", produces = APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public Game game(@PathVariable(value = "id") int id) {
         return Optional.ofNullable(games.get(id)).orElseThrow(() -> new GameNotFoundException(id));
     }
 
-    @PostMapping(value = "/api/game", produces = APPLICATION_JSON_VALUE)
+    @PostMapping(produces = APPLICATION_JSON_VALUE)
     public Game startGame() {
         Game game = new Game(generateId(), new RandomPlayer(), new RockPlayer());
         games.put(game.getId(), game);
         return game;
     }
 
-    @PostMapping(value = "/api/game/{id}/round", produces = APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{id}/round", produces = APPLICATION_JSON_VALUE)
     public Round playRound(@PathVariable(value = "id") int id) {
         Round round = game(id).playRound();
         globalStats.countRound(round);
         return round;
     }
 
-    @DeleteMapping(value = "/api/game/{id}/rounds", produces = APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}/rounds", produces = APPLICATION_JSON_VALUE)
     public Game deleteRounds(@PathVariable(value = "id") int id) {
         return game(id).restartGame();
     }
